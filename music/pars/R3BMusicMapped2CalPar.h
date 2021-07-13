3cd48fbe94a85b9a432fd036c7edc022dc5ff177
Ryo Taniuchi <ryo.taniuchi@york.ac.uk>
2021-07-13 23:07:21 +0200

    R3BMusicMapped2Cal and R3BMusicMapped2CalPar are changed to treat energy dependence on the reconstructed drift time. Position is fitted with three parameters as pos = c0 + c1 * DT + c2 * Ene. Still being under development, keeping a backward compatibility.

--- music/pars/R3BMusicMapped2CalPar.h
+++ music/pars/R3BMusicMapped2CalPar.h
@@ -24,6 +24,7 @@
 #include "R3BMusicMappedData.h"
 #include "R3BMusicMapped2Cal.h"
 #include "TGraph.h"
+#include "TGraph2D.h"
 #include "TH1F.h"
 
 class TClonesArray;
@@ -75,7 +76,7 @@ class R3BMusicMapped2CalPar : public FairTask
   private:
     Int_t fNumAnodes;
     Int_t fMaxMult;
-    Int_t fMinStadistics;
+    Int_t fMinStatistics;
     Int_t fNumParams;
     Int_t fNumPosParams;
     Int_t fNumAnodesRef;
@@ -100,7 +101,8 @@ class R3BMusicMapped2CalPar : public FairTask
     TClonesArray* fHitItemsMwpcA;     /**< Array with hit items. */
     TClonesArray* fHitItemsMwpcB;     /**< Array with hit items. */
 
-    TGraph** fg_anode;
+    TGraph** fg_anode, **fg_anode_result;
+    TGraph2D** fg_anode2d;
 
   public:
     // Class definition
