b7c083098d4b88dabd74d8b470f45ed219abf03d
Ryo Taniuchi <ryo.taniuchi@york.ac.uk>
2021-09-17 18:21:50 +0200

    Offset of pedestal in Enrgy removed in the analysis for the position reconstruction.

--- music/pars/R3BMusicMapped2CalPar.cxx
+++ music/pars/R3BMusicMapped2CalPar.cxx
@@ -324,7 +324,7 @@ void R3BMusicMapped2CalPar::FinishTask()
 	    fit2d->GetParameters(&par[0]);
 	    fCal_Par->SetPosParams(par[0], i * fNumPosParams); // Position
 	    fCal_Par->SetPosParams(par[1], i * fNumPosParams + 1); // Coefficient to DT
-	    fCal_Par->SetPosParams(par[2] + fCal_Par->GetAnodeCalParams()->GetAt(i * fNumParams), i * fNumPosParams + 2); // Coefficient to Energy. Pedestal added for consistency.
+	    fCal_Par->SetPosParams(par[2], i * fNumPosParams + 2); // Coefficient to Energy
 	    //
 	    fg_anode2d[i]->Draw("p");
 	    fit2d->Draw("same");
